3aef1435d7105bb8fe1fed89dd4509292f35816c
Taylor Hale Robert <taylorhale11@gmail.com>
2022-01-23 00:14:12 -0500

    update filepath

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('sample_csa.csv', index_col=0)
+df = pd.read_csv('./data/sample_csa.csv', index_col=0)
 df.drop(['Unnamed: 5', 'Unnamed: 6'], axis=1, inplace=True)
 df.columns = ['fname', 'lname', 'salary', 'eligibility']
 df['eligibility'] = df['eligibility'].apply(lambda x: x.lower())
